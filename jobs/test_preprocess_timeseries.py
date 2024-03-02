bfa0477b1cab63f99f03efbc1eb1cab9d5270112
mlap <marcuslapeyrolerie@me.com>
2024-03-02 21:40:15 +0000

    Adding some accompanying files

---
+++ jobs/test_preprocess_timeseries.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("/home/rstudio/neon4cast-darts-ml/")
+
+from utils import (
+    TimeSeriesPreprocessor,
+)
+import pandas as pd
+import os
+from darts import TimeSeries
+import numpy as np
+import time
+
+if __name__=="__main__":
+    start = time.time()
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    targets = pd.read_csv("targets.csv.gz")
+
+    # For the training set
+    data_preprocessor = TimeSeriesPreprocessor(
+        validation_split_date='2022-07-19',
+        load_dir_name='preprocessed_train/',
+    )
+    
+    data_preprocessor.preprocess_data('FLNT')
+    
+    data_preprocessor.save()
+
+    del data_preprocessor
+
+    data_preprocessor = TimeSeriesPreprocessor(
+        validation_split_date='2022-07-19',
+        load_dir_name='preprocessed_train/',
+    )
+    
+    data_preprocessor.load('FLNT')
+
+    
+    print ("Runtime to preprocess the time series: ", time.time()-start)
