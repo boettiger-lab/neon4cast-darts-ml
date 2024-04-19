42fabf0fcdd20ff60c90f812d8b82dee21ac833b
mlap <marcuslapeyrolerie@me.com>
2024-04-19 00:25:26 +0000

    I think this shouldn't crash cirrus but tbd

--- jobs/run_training.bash
+++ jobs/run_training.bash
@@ -8,3 +8,7 @@
 
 ./jobs/hierarch_train_across.bash chla 1
 ./jobs/hierarch_train_across_nocovs.bash chla 1
+
+./jobs/train_acros_sites_stats.bash oxygen &
+./jobs/train_acros_sites_stats.bash chla &
+./jobs/train_acros_sites_stats.bash temperature &
